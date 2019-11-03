5e570a58bfdd39c3d00459b9655ff0380bf46f81
yumilier19 <1259501451@qq.com>
2019-11-03 19:32:32 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    base: '/blog-demo/',
+    title: 'blog-demo',
+    description: 'Vuepress blog demo',
+    themeConfig: {
+        // 你的GitHub仓库，请正确填写
+        repo: 'https://github.com/yumilier19/blog-demo',
+        // 自定义仓库链接文字。
+        repoLabel: 'My GitHub',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+        ],
+        sidebar: [
+          ['/', '首页'],
+          ['/blog/FirstBlog.md', '我的第一篇博客']
+        ],
+        title: 'group1',
+        collapsable: false,
+        children: [
+            ['/', 'title1'],
+            ['/', 'title2']
+        ],
+        title: 'group2',
+        collapsable: false,
+        children: [
+            ['/', 'title1'],
+        ]
+    }
+  }  
